5f0199d8764c26ee85a3c53249b3acac13ad0a09
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2014-03-21 13:13:38 -0300

    Added date_scraped field

--- scraper.rb
+++ scraper.rb
@@ -51,46 +51,46 @@ if !(defined? Test::Unit::TestCase)
 
   # source: http://www.camara.cl/camara/media/docs/calendario_2014.pdf
   record = Hash.new
-  record = {"id"=>"C001", "title"=>"Semana distrital", "start"=>"2014-03-24", "end"=>"2014-03-28", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx"}
+  record = {"id"=>"C001", "title"=>"Semana distrital", "start"=>"2014-03-24", "end"=>"2014-03-28", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx", "date_scraped"=>"2014-03-21"}
   ScraperWiki.save_sqlite(['id'], record)
 
   record = {}
-  record = {"id"=>"C002", "title"=>"Semana distrital", "start"=>"2014-04-28", "end"=>"2014-05-02", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx"}
+  record = {"id"=>"C002", "title"=>"Semana distrital", "start"=>"2014-04-28", "end"=>"2014-05-02", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx", "date_scraped"=>"2014-03-21"}
   ScraperWiki.save_sqlite(['id'], record)
 
   record = {}
-  record = {"id"=>"C003", "title"=>"Semana distrital", "start"=>"2014-05-26", "end"=>"2014-05-30", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx"}
+  record = {"id"=>"C003", "title"=>"Semana distrital", "start"=>"2014-05-26", "end"=>"2014-05-30", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx", "date_scraped"=>"2014-03-21"}
   ScraperWiki.save_sqlite(['id'], record)
 
   record = {}
-  record = {"id"=>"C004", "title"=>"Semana distrital", "start"=>"2014-06-23", "end"=>"2014-06-27", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx"}
+  record = {"id"=>"C004", "title"=>"Semana distrital", "start"=>"2014-06-23", "end"=>"2014-06-27", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx", "date_scraped"=>"2014-03-21"}
   ScraperWiki.save_sqlite(['id'], record)
 
   record = {}
-  record = {"id"=>"C005", "title"=>"Semana distrital", "start"=>"2014-07-28", "end"=>"2014-08-01", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx"}
+  record = {"id"=>"C005", "title"=>"Semana distrital", "start"=>"2014-07-28", "end"=>"2014-08-01", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx", "date_scraped"=>"2014-03-21"}
   ScraperWiki.save_sqlite(['id'], record)
 
   record = {}
-  record = {"id"=>"C006", "title"=>"Semana distrital", "start"=>"2014-08-25", "end"=>"2014-08-29", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx"}
+  record = {"id"=>"C006", "title"=>"Semana distrital", "start"=>"2014-08-25", "end"=>"2014-08-29", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx", "date_scraped"=>"2014-03-21"}
   ScraperWiki.save_sqlite(['id'], record)
 
   record = {}
-  record = {"id"=>"C007", "title"=>"Semana distrital", "start"=>"2014-09-15", "end"=>"2014-09-19", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx"}
+  record = {"id"=>"C007", "title"=>"Semana distrital", "start"=>"2014-09-15", "end"=>"2014-09-19", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx", "date_scraped"=>"2014-03-21"}
   ScraperWiki.save_sqlite(['id'], record)
 
   record = {}
-  record = {"id"=>"C008", "title"=>"Semana distrital", "start"=>"2014-10-27", "end"=>"2014-10-31", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx"}
+  record = {"id"=>"C008", "title"=>"Semana distrital", "start"=>"2014-10-27", "end"=>"2014-10-31", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx", "date_scraped"=>"2014-03-21"}
   ScraperWiki.save_sqlite(['id'], record)
 
   record = {}
-  record = {"id"=>"C009", "title"=>"Semana distrital", "start"=>"2014-12-01", "end"=>"2014-12-05", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx"}
+  record = {"id"=>"C009", "title"=>"Semana distrital", "start"=>"2014-12-01", "end"=>"2014-12-05", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx", "date_scraped"=>"2014-03-21"}
   ScraperWiki.save_sqlite(['id'], record)
 
   record = {}
-  record = {"id"=>"C010", "title"=>"Semana distrital", "start"=>"2014-12-22", "end"=>"2015-01-02", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx"}
+  record = {"id"=>"C010", "title"=>"Semana distrital", "start"=>"2014-12-22", "end"=>"2015-01-02", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx", "date_scraped"=>"2014-03-21"}
   ScraperWiki.save_sqlite(['id'], record)
 
   record = {}
-  record = {"id"=>"C011", "title"=>"Semana distrital", "start"=>"2015-01-26", "end"=>"2015-01-30", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx"}
+  record = {"id"=>"C011", "title"=>"Semana distrital", "start"=>"2015-01-26", "end"=>"2015-01-30", "chamber"=>"C.Diputados", "url"=>"http://www.camara.cl/camara/distritos.aspx", "date_scraped"=>"2014-03-21"}
   ScraperWiki.save_sqlite(['id'], record)
 end
